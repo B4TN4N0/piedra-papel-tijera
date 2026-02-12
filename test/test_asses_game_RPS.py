030bd158da09a551f168625f2f52fad8ec38187a
ccastroto <castroaldrovercarlossangrinha@gmail.com>
2026-02-12 09:47:02 +0100

    test(rps): Comentar los csos test

--- test/test_asses_game_RPS.py
+++ test/test_asses_game_RPS.py
@@ -19,56 +19,56 @@ def test_draw():
         user_action=GameAction.Paper,
         computer_action=GameAction.Paper)
 
-@pytest.mark.rock
-def test_rock_loses():
-    '''
-    Rock pierde con Paper 
-    '''
-    assert GameResult.Victory == assess_game(
-        user_action=GameAction.Paper,
-        computer_action=GameAction.Rock)
-
-@pytest.mark.rock
-def test_rock_wins():
-    '''
-    Rock gana a Scissors
-    '''
-    assert GameResult.Defeat == assess_game(
-        user_action=GameAction.Scissors,
-        computer_action=GameAction.Rock)
-
-@pytest.mark.paper
-def test_paper_loses():
-    '''
-    Paper pierde con Scissors
-    '''
-    assert GameResult.Victory == assess_game(
-        user_action=GameAction.Scissors,
-        computer_action=GameAction.Paper)
-
-@pytest.mark.paper
-def test_paper_wins():
-    '''
-    Paper gana a Rock
-    '''
-    assert GameResult.Defeat == assess_game(
-        user_action=GameAction.Rock,
-        computer_action=GameAction.Paper)
-
-@pytest.mark.scissors
-def test_scissors_loses():
-    '''
-    Scissors pierde con Rock 
-    '''
-    assert GameResult.Victory == assess_game(
-        user_action=GameAction.Rock,
-        computer_action=GameAction.Scissors)
-
-@pytest.mark.scissors
-def test_scissors_wins():
-    '''
-    Scissors gana a Paper 
-    '''
-    assert GameResult.Defeat == assess_game(
-        user_action=GameAction.Paper,
-        computer_action=GameAction.Scissors)
+#@pytest.mark.rock
+#def test_rock_loses():
+#    '''
+#    Rock pierde con Paper 
+#    '''
+#    assert GameResult.Victory == assess_game(
+#        user_action=GameAction.Paper,
+#        computer_action=GameAction.Rock)
+#
+#@pytest.mark.rock
+#def test_rock_wins():
+#    '''
+#    Rock gana a Scissors
+#    '''
+#    assert GameResult.Defeat == assess_game(
+#        user_action=GameAction.Scissors,
+#        computer_action=GameAction.Rock)
+#
+#@pytest.mark.paper
+#def test_paper_loses():
+#    '''
+#    Paper pierde con Scissors
+#    '''
+#    assert GameResult.Victory == assess_game(
+#        user_action=GameAction.Scissors,
+#        computer_action=GameAction.Paper)
+#
+#@pytest.mark.paper
+#def test_paper_wins():
+#    '''
+#    Paper gana a Rock
+#    '''
+#    assert GameResult.Defeat == assess_game(
+#        user_action=GameAction.Rock,
+#        computer_action=GameAction.Paper)
+#
+#@pytest.mark.scissors
+#def test_scissors_loses():
+#    '''
+#    Scissors pierde con Rock 
+#    '''
+#    assert GameResult.Victory == assess_game(
+#        user_action=GameAction.Rock,
+#        computer_action=GameAction.Scissors)
+#
+#@pytest.mark.scissors
+#def test_scissors_wins():
+#    '''
+#    Scissors gana a Paper 
+#    '''
+#    assert GameResult.Defeat == assess_game(
+#        user_action=GameAction.Paper,
+#        computer_action=GameAction.Scissors)
